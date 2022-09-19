0f435c58a02f567cad7525de28a1fd2d913478e8
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-19 21:59:16 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * swap_int - swap two pointer variables
+ *@a: First pointer variable
+ *@b: Second pointer variable
+ *
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+  int i = 0;
+  
+	while (*(s+i) != '\0')
+	{
+		i++;
+	}
+	return i;
+}
