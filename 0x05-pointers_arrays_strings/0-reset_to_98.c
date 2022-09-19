7d640b9de8d40391679e1111495ef73228f570b7
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-19 21:37:04 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * reset_to_98 - Reset number to 98
+* reset_to_98 - Reset number to 98
 *@n: Pointer to variable to be reset 
- *
- * Return: Nothing.
- */
+*
+* Return: Nothing.
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
