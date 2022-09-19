72ee52da72ee10a87e0aada8ee0ba7137fa919e0
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-19 22:16:24 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/**
+ * _putchar - check the code
+ *@str: Variable containing string
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (*(str + i) != '\0')
+{
+_putchar(*(str + i));
+i++;
+}
+_putchar('\n');
+}
