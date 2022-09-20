01c46c4f93c878fb6353cd04829be7ff243319dd
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-20 01:13:34 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,26 @@
 void puts2(char *str)
 {
 	int i = 0;
-	while (*(str + i) != '\0')
+	while (i < _strlen(str)
 	{
 		_putchar(*(str + i));
 		i += 2;
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - Returns Length of strings
+ *@s: Pointer variable points to the string
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (*(s + i) != '\0')
+{
+i++;
+}
+return (i);
 }
