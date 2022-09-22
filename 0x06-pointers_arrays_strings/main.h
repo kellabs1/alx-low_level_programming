4ea1cbd6182845d49ebf05bb32b787c519a037c3
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-22 15:15:20 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,5 @@ char *_strcat(char *dest, char *src);
 char *cap_string(char *);
 char *string_toupper(char *);
 int _putchar(char c);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
