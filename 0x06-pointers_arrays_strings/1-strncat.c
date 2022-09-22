f869c0b993ff23e63f6871a8f7be54f0489d566b
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-22 14:12:17 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
  * _strncat - concatenate src to dest
- * @dest: base pointer variable 
+ * @dest: base pointer variable
  * @src: pointer variable to append to dest
  * @n: number of bytes from src to append to dest
  *
@@ -12,9 +11,13 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
+
 	i = j = 0;
+
 	while (*(dest + i++))
+	{
 		;
+	}
 	while ((*(dest + i++) = *(dest + j++)) && j <= n)
 	{
 		;
