fa906d1091c347a1d84a82785cacb3b85447ed4d
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-22 14:33:34 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,13 @@
 
 char *string_toupper(char *s)
 {
-	int i = 0;
+int i = 0;
 
-	while (*(s + i))
-	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-			*(s + i) -= 'a' - 'A';
-		i++;
-	}
-	return (s);
+while (*(s + i))
+{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+*(s + i) -= 'a' - 'A';
+i++;
+}
+return (s);
 }
