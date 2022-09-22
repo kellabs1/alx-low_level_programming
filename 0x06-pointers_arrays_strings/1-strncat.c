cdf5f0cd47f0eea1d76bce5d6b4234b0dc29e0db
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-22 14:15:29 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,20 +7,20 @@
  *
  * Return: address of dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
 	i = j = 0;
-
-	while (*(dest + i++))
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
 	{
-		;
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
 	}
-	while ((*(dest + i++) = *(dest + j++)) && j <= n)
-	{
-		;
-	}
+	if (j < n)
+		*(dest + i) = *(src + j);
 	return (dest);
 }
