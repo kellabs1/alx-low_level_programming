429af44b6efebd49f9c15be687c45f17c300e6a8
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-27 00:46:07 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-char _putchar(char s); 
+int _putchar(char c); 
 void print_diagsums(int *a, int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
