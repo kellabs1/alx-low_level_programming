4a2f26565a2b38fa59d6c3874374078ac63b9377
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-27 02:37:59 +0100

    Create 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *print_chessboard - prints chessboard ax8
+ *
+ *@a: 2d array
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	char p;
+	i = 0;
+
+	while (i < 8)
+	{
+		j = 0;
+
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
