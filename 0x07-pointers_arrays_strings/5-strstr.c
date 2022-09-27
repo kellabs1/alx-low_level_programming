3a89270a92273bf35aeaa947dc8ae9f8c6c21fe9
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-27 02:33:05 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,24 +8,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-	int j;
-
-	i = 0;
+	int i = 0, j = 0;
 
 	while (haystack[i] != '\0')
 	{
-		j = 0;
-
-		while (needle[j] != '\0')
+		while (haystack[i + j] == needle[j])
 		{
-			if (haystack[i] == needle[j + 1])
-			{
-				return haystack;
-			}
-			*haystack++;
 			j++;
 		}
+
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+
 		i++;
 	}
 	return (0);
