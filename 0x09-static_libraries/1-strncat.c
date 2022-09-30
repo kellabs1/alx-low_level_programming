dc57c5019c4f49ab6beb164209fd5f536abddb71
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-30 09:53:14 +0100

    Create 1-strncat.c

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - concatenate src to dest
+ * @dest: base pointer variable
+ * @src: pointer variable to append to dest
+ * @n: number of bytes from src to append to dest
+ *
+ * Return: address of dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	if (j < n)
+		*(dest + i) = *(src + j);
+	return (dest);
+}
