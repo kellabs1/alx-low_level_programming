b9496e4bf847954ca1dd51c6ef1524ee67510b9c
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-30 09:54:51 +0100

    Create 2-strncpy.c

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n bytes from src to dest
+ * @dest: string variable to copy to
+ * @src: string variable to be copied
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
