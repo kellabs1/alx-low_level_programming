b70edd4a8b18f7065adf1186910083f8e64e6cf1
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-30 09:50:59 +0100

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concatenate src with dest string
+ * @dest: string pointer to serve as base
+ * @src: string pointer variable to append to dest
+ *
+ * Return: address of dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+
+i = j = 0;
+
+while (*(dest + i))
+{
+i++;
+}
+while ((*(dest + i) = *(src + j)))
+{
+i++;
+j++;
+}
+return (dest);
+}
