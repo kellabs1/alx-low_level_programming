d015ad8c5f92fe1885ecd61e71ec34f4e76bd082
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-09-30 09:28:03 +0100

    Create 9-strcpy.c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - check the code
+ *@dest: Variable to be copied to
+ *@src: Variable from where to copy from
+ *
+ * Return: Pointer to string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while ((*(dest + i) = *src++))
+	{
+		i++;
+	}
+	return (dest);
+
+}
